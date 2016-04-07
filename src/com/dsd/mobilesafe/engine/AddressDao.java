9a48869a4f3849e9d604137616d2d06e02377662
im_dsd <im_dsd@126.com>
2016-04-08 00:30:10 +0800

    用正则表达式判读电话号码之完成了10分钟 明天继续，但是明天应该是学不了多少了

--- src/com/dsd/mobilesafe/engine/AddressDao.java
+++ src/com/dsd/mobilesafe/engine/AddressDao.java
@@ -18,35 +18,50 @@ public class AddressDao {
 	 *            需要查询的电话号码
 	 */
 	public static void getAdress(String phone) {
-		File file = new File(path);
-		if (file.exists()) {
-			Log.i(tag, "address.db存在");
-			phone = phone.substring(0, 7);
-			// [2]传递电话号码，开启数据库链接，进行访问，返回一个归属地
-			// 【2.1】开始数据库链接，（只读的形式打开）
-			SQLiteDatabase db = SQLiteDatabase.openDatabase(path, null,
-					SQLiteDatabase.OPEN_READONLY);
-			// [3]查询数据库
-			Cursor cursor = db.query("data1", new String[] { "outkey" },
-					"id = ?", new String[] { phone }, null, null, null);
-			// [4]查到即可
-			while (cursor.moveToNext()) {
-				String outKey = cursor.getString(0);
-				Log.i(tag, "outKey : " + outKey);
 
-				// [5]使用outkey作为键查询data2表
-
-				Cursor indexCursof = db.query("data2", new String[] { "location" },
-						"id = ?", new String[] { outKey }, null, null, null);
+		// 正则标的式，匹配手机号码
+		// 手机的正则的表达式
+		String regularExpression = "^1[3-8]\\d{9}";
+		if (phone.matches(regularExpression)) {
+			File file = new File(path);
+			if (file.exists()) {
 				
-				while(indexCursof.moveToNext())
-				{
-					//[6]获取电话归属地
-					String address = indexCursof.getString(0);
-					Log.i(tag, "address : " + address);
-				}
+				phone = phone.substring(0, 7);
+				// [2]传递电话号码，开启数据库链接，进行访问，返回一个归属地
+				// 【2.1】开始数据库链接，（只读的形式打开）
+				SQLiteDatabase db = SQLiteDatabase.openDatabase(path, null,
+						SQLiteDatabase.OPEN_READONLY);
+				// [3]查询数据库
+				Cursor cursor = db.query("data1", new String[] { "outkey" },
+						"id = ?", new String[] { phone }, null, null, null);
+				// [4]查到即可
+				while (cursor.moveToNext()) {
+					String outKey = cursor.getString(0);
+					Log.i(tag, "outKey : " + outKey);
 
+					// [5]使用outkey作为键查询data2表
+
+					Cursor indexCursof = db.query("data2",
+							new String[] { "location" }, "id = ?",
+							new String[] { outKey }, null, null, null);
+
+					while (indexCursof.moveToNext()) {
+						// [6]获取电话归属地
+						String address = indexCursof.getString(0);
+						Log.i(tag, "address : " + address);
+					}
+
+				}
 			}
+			else
+			{
+				Log.i(tag, "address.db不存在");
+			}
+		}
+		else
+		{
+			int length = phone.length();
+			
 		}
 
 	}
